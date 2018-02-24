5b68e9a99a1cc924b302d5cf017f01b4e13e60f6
hell <1339273338@qq.com>
2018-02-24 11:38:18 +0800

    添加modular表数据

---
+++ src/main/java/mysql/dml/modular_dml.sql
@@ -0,0 +1,14 @@
+
+--------- 2018/2/22 开始----------------
+
+INSERT INTO modular(id,name,remarks) VALUES (1,'分享·生活录','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (2,'分享·图片','用图片分享你美好的生活');
+INSERT INTO modular(id,name,remarks) VALUES (3,'分享·短视频','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (4,'分享·旅行记','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (5,'分享·文学创作','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (6,'分享·教育','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (7,'分享·育儿经','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (8,'分享·原创小说','分享你的生活录');
+INSERT INTO modular(id,name,remarks) VALUES (9,'话题分享','分享你的生活录');
+
+--------- 2018/2/22 结束----------------
