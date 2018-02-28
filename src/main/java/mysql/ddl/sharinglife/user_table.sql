bad2965a482c96b05c160e5509905e9777fdda44
hell <1339273338@qq.com>
2018-02-28 16:31:18 +0800

    修改表

--- src/main/java/mysql/ddl/sharinglife/user_table.sql
+++ src/main/java/mysql/ddl/sharinglife/user_table.sql
@@ -9,11 +9,14 @@ CREATE TABLE `user`(
   `email` VARCHAR(30)  COMMENT '用户邮箱',
   `type` tinyint(4) unsigned NOT NULL DEFAULT 1 COMMENT '用户类型：1普通用户、2会员、3管理员、4超级用户',
   `sharecoin` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '用户分享币',
-  `motto` VARCHAR(30) COMMENT '用户格言',
+  `motto` VARCHAR(300) COMMENT '用户格言',
+  `lastLoginIp` VARCHAR(20)  COMMENT '最后登陆的ip',
+  `avatarUrl` VARCHAR(30)  COMMENT '头像图片路径',
   `createTime` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '用户创建时间',
   `updateTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户更新时间',
   PRIMARY KEY (`id`),
-  KEY `phone_index` (`phone`) USING BTREE
+  KEY `phone_index` (`phone`) USING BTREE,
+  KEY `name_index` (`name`) USING BTREE
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 ----- rollback  ------
